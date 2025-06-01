8c4103be974a50eafe078f3ec20ff38e09f33322
agweb12 <graziani1112@gmail.com>
2025-06-01 18:58:12 +0200

    version 1.0 correction header headeradmin

--- app/views/header.php
+++ app/views/header.php
@@ -39,28 +39,26 @@
     <link rel="canonical" href="<?= RACINE_SITE ?>">
 
     <!-- Favicon -->
-    <link rel="shortcut icon" href="<?= RACINE_SITE ?>favicon.ico" type="image/x-icon">
-    <link rel="apple-touch-icon" sizes="57x57" href="<?= RACINE_SITE ?>apple-icon-57x57.png">
-    <link rel="apple-touch-icon" sizes="60x60" href="<?= RACINE_SITE ?>apple-icon-60x60.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="<?= RACINE_SITE ?>apple-icon-72x72.png">
-    <link rel="apple-touch-icon" sizes="76x76" href="<?= RACINE_SITE ?>apple-icon-76x76.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="<?= RACINE_SITE ?>apple-icon-114x114.png">
-    <link rel="apple-touch-icon" sizes="120x120" href="<?= RACINE_SITE ?>apple-icon-120x120.png">
-    <link rel="apple-touch-icon" sizes="144x144" href="<?= RACINE_SITE ?>apple-icon-144x144.png">
-    <link rel="apple-touch-icon" sizes="152x152" href="<?= RACINE_SITE ?>apple-icon-152x152.png">
-    <link rel="apple-touch-icon" sizes="180x180" href="<?= RACINE_SITE ?>apple-icon-180x180.png">
-    <link rel="icon" type="image/png" sizes="36x36" href="<?= RACINE_SITE ?>android-icon-36x36.png">
-    <link rel="icon" type="image/png" sizes="48x48" href="<?= RACINE_SITE ?>android-icon-48x48.png">
-    <link rel="icon" type="image/png" sizes="72x72" href="<?= RACINE_SITE ?>android-icon-72x72.png">
-    <link rel="icon" type="image/png" sizes="96x96" href="<?= RACINE_SITE ?>android-icon-96x96.png">
-    <link rel="icon" type="image/png" sizes="144x144" href="<?= RACINE_SITE ?>android-icon-144x144.png">
-    <link rel="icon" type="image/png" sizes="192x192" href="<?= RACINE_SITE ?>android-icon-192x192.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="<?= RACINE_SITE ?>favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="96x96" href="<?= RACINE_SITE ?>favicon-96x96.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="<?= RACINE_SITE ?>favicon-16x16.png">
-    <link rel="manifest" href="<?= RACINE_SITE ?>manifest.json">
+    <link rel="shortcut icon" href="<?= RACINE_SITE ?>public/favicon.ico" type="image/x-icon">
+    <link rel="apple-touch-icon" sizes="57x57" href="<?= RACINE_SITE ?>public/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="<?= RACINE_SITE ?>public/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="<?= RACINE_SITE ?>public/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="<?= RACINE_SITE ?>public/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="<?= RACINE_SITE ?>public/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="<?= RACINE_SITE ?>public/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="<?= RACINE_SITE ?>public/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="<?= RACINE_SITE ?>public/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="<?= RACINE_SITE ?>public/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="36x36" href="<?= RACINE_SITE ?>public/android-icon-36x36.png">
+    <link rel="icon" type="image/png" sizes="48x48" href="<?= RACINE_SITE ?>public/android-icon-48x48.png">
+    <link rel="icon" type="image/png" sizes="72x72" href="<?= RACINE_SITE ?>public/android-icon-72x72.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="<?= RACINE_SITE ?>public/android-icon-96x96.png">
+    <link rel="icon" type="image/png" sizes="144x144" href="<?= RACINE_SITE ?>public/android-icon-144x144.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="<?= RACINE_SITE ?>public/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="<?= RACINE_SITE ?>public/favicon-96x96.png">
+    <link rel="manifest" href="<?= RACINE_SITE ?>public/manifest.json">
     <meta name="msapplication-TileColor" content="#ffffff">
-    <meta name="msapplication-TileImage" content="<?= RACINE_SITE ?>ms-icon-144x144.png">
+    <meta name="msapplication-TileImage" content="<?= RACINE_SITE ?>public/ms-icon-144x144.png">
     <meta name="theme-color" content="#ffffff">
     <link href="<?= RACINE_SITE ?>public/assets/icons/webfonts/uicons-solid-rounded.css" rel="stylesheet">
     <link href="<?= RACINE_SITE ?>public/assets/icons/webfonts/uicons-brands.css" rel="stylesheet">
